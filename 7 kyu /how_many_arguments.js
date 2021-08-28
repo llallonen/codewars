b352d1fac4ad13e4da9abfbdc24f66c10a71af43
llallonen <llallonen@gmail.com>
2021-08-29 01:01:46 +0300

    three array and string  themes

---
+++ 7 kyu /how_many_arguments.js
@@ -0,0 +1,6 @@
+// Create a function called args_count that returns the number of arguments provided
+
+//args_count(1, 2, 3) -> 3
+//args_count(1, 2, 3, 10) -> 4
+
+const args_count = (...args) => { return args.length; }
